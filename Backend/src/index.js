8d60076b4bd301d2d67233eeabfdeb8304b34075
Developer866 <opeyemijoseph866@gmail.com>
2026-01-09 19:16:06 +0100

    feat: Add database connection logic and update server startup process

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,18 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import app from './app.js';
+import connectDB from './config/database.js';
 // import cors from 'cors';
 
 dotenv.config({path:'./.env'});
 
-const startServer =()=>{
+const startServer = async () => {
     try {
+        await connectDB(process.env.MONGODB_URI);
         app.listen(process.env.port, () => {
             console.log(`Server is running on port ${process.env.port}`);
         })
     }
     catch (error) {
         console.error('Error starting server:', error);
+        process.exit(1);
     }
 }
 startServer();
